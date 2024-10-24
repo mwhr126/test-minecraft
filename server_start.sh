5e564db8b08f71d5a70ec7ef9b753ef4002dae46
miwa <dskipineapple@gmail.com>
2024-10-24 11:28:26 +0900

    点数加算のマルチプレイヤー対応

--- server_start.sh
+++ server_start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-cd "$(dirname "$0")"
-java -Xms4G -Xmx4G -XX:+UseG1GC -jar paper-server.jar nogui
+cd "$( dirname "$0" )"
+java -Xms4G -Xmx4G -XX:+UseG1GC -jar -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=5005 paper-server.jar nogui
